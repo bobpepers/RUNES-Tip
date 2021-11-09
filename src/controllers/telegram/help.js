5ca56cd5afd2fb0a62957429f98b43dacdb3e5d1
bobpepers <bob.pepers@gmail.com>
2021-11-09 18:58:32 +0100

    telegram help conditional

--- src/controllers/telegram/help.js
+++ src/controllers/telegram/help.js
@@ -6,12 +6,21 @@ require('dotenv').config();
 const { Markup } = require('telegraf');
 
 export const fetchHelp = async (ctx) => {
-  ctx.replyWithHTML(helpMessage(), Markup.inlineKeyboard([
-    [Markup.button.callback('Balance', 'Balance'),
-      Markup.button.callback('Price', 'Price')],
-    [Markup.button.callback('Exchanges', 'Exchanges'),
-      Markup.button.callback('Deposit', 'Deposit')],
-    [Markup.button.callback('Referral', 'Referral'),
-      Markup.button.callback('Referral Top 10', 'ReferralTop')],
-  ]));
+  ctx.replyWithHTML(helpMessage(), Markup.inlineKeyboard(
+    process.env.CURRENCY_NAME === 'Runebase'
+      ? [
+        [Markup.button.callback('Balance', 'Balance'),
+          Markup.button.callback('Price', 'Price')],
+        [Markup.button.callback('Exchanges', 'Exchanges'),
+          Markup.button.callback('Deposit', 'Deposit')],
+        [Markup.button.callback('Referral', 'Referral'),
+          Markup.button.callback('Referral Top 10', 'ReferralTop')],
+      ]
+      : [
+        [Markup.button.callback('Balance', 'Balance'),
+          Markup.button.callback('Price', 'Price')],
+        [Markup.button.callback('Exchanges', 'Exchanges'),
+          Markup.button.callback('Deposit', 'Deposit')],
+      ],
+  ));
 };
