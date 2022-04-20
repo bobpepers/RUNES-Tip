df5c2082185af42eabc00aee8272629db8a93c64
Bob Pepers <bob.pepers@gmail.com>
2022-04-20 09:10:33 +0200

    remove hardcoded secret for verification tokens

--- src/dashboard/helpers/generate.js
+++ src/dashboard/helpers/generate.js
@@ -1,15 +1,17 @@
 import bcrypt from 'bcrypt-nodejs';
+import { config } from "dotenv";
+import crypto from "crypto";
 
-const crypto = require('crypto');
+config();
 
-export const generateHash = (a) => crypto.createHmac('sha256', 'SuperSexySecret').update(a).digest('hex');
+export const generateHash = (a) => crypto.createHmac('sha256', process.env.SESSION_SECRET).update(a).digest('hex');
 
 // eslint-disable-next-line import/prefer-default-export
 export const generateVerificationToken = (expireHours) => {
   const generateToken = new Promise((resolve, reject) => {
     bcrypt.genSalt(10, (err, salt) => {
       if (err) reject(err);
-      bcrypt.hash('SuperSexySecret', salt, null, (err, hash) => {
+      bcrypt.hash(process.env.SESSION_SECRET, salt, null, (err, hash) => {
         if (err) reject(err);
         const expires = new Date();
         expires.setHours(expires.getHours() + expireHours);
